9948f1a15f391cb94fe9a3cd93cd5870ef67577d
Simonwristel <Simon.wristel@elev.ga.ntig.se>
2023-11-09 12:43:05 +0100

    I changed the name of the variables

--- projekt1.py
+++ projekt1.py
@@ -12,30 +12,30 @@ print(bcolors.CYAN + """
 |-----------------------|
 |_______________________|\n\n""")
     
-secret_number = random.randint(1,100)
-total_guesses = 0
+random_number = random.randint(1,100)
+guesses = 0
 
-while total_guesses <= 7:
+while guesses <= 7:
     try:
         guess = int(input(bcolors.GREEN + "Gissa det hemliga talet: "))
     except ValueError: 
         print(bcolors.RED + "Du måste skriva siffror och inga bokstäver!!")
         continue
 
-    total_guesses += 1
+    guesses += 1
 
-    if guess > secret_number:
-        print("För högt, skriv lägre tal")
+    if guess > random_number:
+        print(bcolors.BLUE + "För högt, skriv lägre tal")
 
-    elif guess < secret_number:
+    elif guess < random_number:
         print(bcolors.YELLOW + "För lågt, skriv ett högre tal")
         
-    elif guess == secret_number:
-        print(bcolors.PURPLE + f'\nGrattis du har hittat talet! {secret_number} på  + {total_guesses} försök!\n\n')
+    elif guess == random_number:
+        print(bcolors.PURPLE + f'\nGrattis du har hittat talet! {random_number} på  + {guesses} försök!\n\n')
         break
         
-    if total_guesses == 7:
-        print(bcolors.UNDERLINE + bcolors.BLUE + f'\nTyvärr, du hittade inte det hemliga talet: {secret_number}.')
+    if guesses == 7:
+        print(bcolors.UNDERLINE + bcolors.BLUE + f'\nTyvärr, du hittade inte det hemliga talet: {random_number}.')
         break
     
         
